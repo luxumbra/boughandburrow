66d581a50a2a32560dcdd34087e82e956f73dbb2
luxumbra <me@luxumbra.dev>
2025-01-26 20:54:46 +0000

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,31 @@
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import keystatic from "@keystatic/astro";
+import netlify from "@astrojs/netlify";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import compress from "@playform/compress";
+
+// https://astro.build/config
+export default defineConfig({
+	site: "https://boughndburrow.uk/",
+	adapter: netlify({
+		imageCDN: false,
+	}),
+	redirects: {
+		"/admin": "/keystatic",
+	},
+	integrations: [
+		react(),
+		keystatic(),
+		tailwind(),
+		sitemap(),
+		compress({
+			HTML: true,
+			JavaScript: true,
+			CSS: true,
+			Image: false, // astro:assets handles this. Enabling this can dramatically increase build times
+			SVG: false, // astro-icon handles this
+		}),
+	],
+});
---
+++ src/data/bio/index.md
@@ -0,0 +1,9 @@
+---
+name: Bough & Burrow
+theme: dark
+blur: blur
+avatar: ./avatar.jpg
+background: ./background.jpg
+---
+Handcrafted woodland-inspired treasures made with love and care. From whimsical gonks to rustic woodwork, we bring the charm of nature into your home. 🌿 🌳 🌍
+Based in the UK.
